bbb769c31f0734e5a494e52c69e040314073f772
Ron Beshears <ron.beshears@gmail.com>
2015-06-24 16:07:42 -0700

    Portfolio Site

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,33 @@
 $(document).ready(function(){
 	console.log('dom ready');
+	//all code here
 	
-	//var render = function ();
-	//why a for loop?
-	//requetAnimationFrame cross browser
-	//cross browser css animation
-	//why not margin-top?
+	//my current projects
+	$(".project-pics").hide();
+	$("#web-images").on("click", "button", function() {
+       $(".project-pics").slideToggle();
+  });
+  	$(".project-pics").on("mouseenter", "li", function() {
+    $(this).find("span").slideToggle();
+  });
 
-	var $container = $(".parallax");
-	var $divs = $container.find("div.parallax-background");
-	var thingBeingScrolled = document.body;
-	var liHeight = $divs.eq(0).closest("li").height();
-	var diffHeight = $divs.eq(0).height() - liHeight;
-	var len = $divs.length;
+  	//badge logos
+	$(".badge-logo").hide();
+	$("#web-badges").on("click", "button", function() {
+       $(".badge-logo").slideToggle();
+  });
+  	$(".badge-logo").on("mouseenter", "li", function() {
+    $(this).find("span").slideToggle();
+  });
 
-
-	var i,len,div,li,offset,scroll,top,transform;
-
-	// cache initial offsets
-	var offsets = $divs.get().map(function(div,d) {
-		return $(div).offset();
-	});
-
-	var render = function() {
-
-		top = thingBeingScrolled.scrollTop;
-
-		for(i=0;i<len;i++) {
-			//get the DOM object
-			div = $divs[i];
-		
-			// our offset
-			offset = top - offsets[i].top;
-
-		//our transform string
-		scroll = ~~(offset / liHeight * diffHeight);
-
-		//our transform string
-		transform = 'translate3d(0px, ' + scroll + 'px, 0px)';
-
-		//apply
-		div.style.webkitTransform = transform;
-		div.style.MozTransform = transform;
-		div.style.msTransform = transform;
-		div.style.OTransform = transform;
-		div.style.transform = transform;
+  	//skill set
+	$(".skills-content").hide();
+	$("#web-skills").on("click", "button", function() {
+       $(".skills-content").slideToggle();
+  });
+  	$(".skills-content").on("mouseenter", "li", function() {
+    $(this).find("span").slideToggle();
+  });
 
 
-
-		}
-
-
-	};
-
-	(function loop(){
-				requestAnimationFrame(loop);
-				render();
-			})();
-
-
-			(function() {
-		    var lastTime = 0;
-		    var vendors = ['webkit', 'moz'];
-		    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-		        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-		        window.cancelAnimationFrame =
-		          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-		    }
-
-		    if (!window.requestAnimationFrame)
-		        window.requestAnimationFrame = function(callback, element) {
-		            var currTime = new Date().getTime();
-		            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-		            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-		              timeToCall);
-		            lastTime = currTime + timeToCall;
-		            return id;
-		        };
-
-		    if (!window.cancelAnimationFrame)
-		        window.cancelAnimationFrame = function(id) {
-		            clearTimeout(id);
-		        };
-}());
-})
+});
